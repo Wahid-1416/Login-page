7ae1c77369045ba592d58eb36963d6aadc51229b
Wahid-1416 <147654913+Wahid-1416@users.noreply.github.com>
2024-01-29 17:09:21 +0530

    dot left

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-var t1=gsap.timeline()
 
-t1.from("nav",{
-    opacity:0,
-    duration:.5,
-    delay:1
+function tl() {
     
-})
-t1.from("#logo",{
-    y:-100,
-    opacity:0,
-    duration:1
-})
-t1.from("#nav-part2",{
-    y:-100,
-    opacity:0,
-    duration:1,
-})
+    var t1=gsap.timeline()
+    t1.from("nav",{
+        opacity:0,
+        duration:.5,
+        delay:1
+        
+    })
+    t1.from("#logo",{
+        y:-100,
+        opacity:0,
+        duration:1
+    })
+    t1.from("#nav-part2",{
+        y:-100,
+        opacity:0,
+        duration:1,
+    })
+    
+    t1.from("#particles-js",{
+        opacity:0,
+        duration:.5
+    })
+    
+    t1.from("#box",{
+        scale:0,
+        opacity:0,
+        duration:1
+    })
+    t1.from("#but h3",{
+        scale:2,
+        opacity:0,
+        duration:1
+    })
+    
+}
 
-t1.from("#particles-js",{
-    opacity:0,
-    duration:.5
-})
+tl()
 
-t1.from("#box",{
-    scale:0,
-    opacity:0,
-    duration:1
-})
-t1.from("#but h3",{
-    scale:2,
-    opacity:0,
-    duration:1
-})
+
