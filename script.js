53a47fa88df909a2a83e57e96e939c4795c4c330
Wahid-1416 <147654913+Wahid-1416@users.noreply.github.com>
2024-01-29 21:24:47 +0530

    Done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 function tl() {
     
     var t1=gsap.timeline()
+    
+    
     t1.from("nav",{
         opacity:0,
         duration:.5,
@@ -40,3 +42,50 @@ function tl() {
 tl()
 
 
+
+
+var qu= document.querySelector("#particles-js");
+var circle=document.querySelectorAll(".circle");
+var box=document.querySelector("#box");
+
+console.log(circle);
+qu.addEventListener("mousemove",function(){
+    
+    for (let c of circle) {
+        console.log(c);
+        c.style.display="Block";
+      }
+})
+
+
+box.addEventListener("mouseout",function(){
+    
+    for (let c of circle) {
+        console.log(c);
+        c.style.display="Block";
+      }
+})
+
+
+qu.addEventListener("mousemove",function(details){
+    
+    let X=details.clientX;
+    let Y=details.clientY;
+
+    for (let c of circle) {
+        c.style.left=String(X)+"px";
+        c.style.top=String(Y)+"px";
+        
+      }
+
+})
+
+
+
+box.addEventListener("mouseover",function(details){
+    for (let c of circle) {
+        c.style.display="none";
+        
+      }
+
+})
